fab221b8cc10522a8c277bcbc495d41e6e9a9fb9
gresba <kimpaul0709@gmail.com>
2022-06-27 16:36:37 -0400

    Finished Search Bar

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let apiRoot = "https://dev.sellix.io/v1";
 
 let endPoint = '/categories';
 
+document.querySelector("#search-bar").addEventListener("keyup", search);
+
 fetch(`${apiRoot}${endPoint}`, {
   headers: {
     'Authorization': 'Bearer V2Mu5ZKnY0zLAOIrJLprSRdwmru6nR75BTsPcIEq1B6FRNYVfevZpYz7p56TERFz'
@@ -12,7 +14,6 @@ fetch(`${apiRoot}${endPoint}`, {
   );
 
 function appendProducts(json) {
-  console.log(json)
   json.data.categories.forEach((category) => {
     const main = document.querySelector("main");
 
@@ -32,8 +33,6 @@ function appendProducts(json) {
       appendP(product.uniqid, productContainer);
       main.appendChild(productContainer)
     });
-
-
   });
 }
 
@@ -45,25 +44,48 @@ async function appendP(productId, productContainer) {
     }
   }).then(result => result.json())
     .then(result => {
-      console.log(result)
+      const product = result.data.product;
 
       let productDiv = document.createElement("div");
+      productDiv.dataset.title = product.title;
       productDiv.classList.add("product-div");
 
       let image = document.createElement("img");
-      let cloudFareImageId = result.data.product.cloudflare_image_id;
+      let cloudFareImageId = product.cloudflare_image_id;
       image.src = `https://imagedelivery.net/95QNzrEeP7RU5l5WdbyrKw/${cloudFareImageId}/public`
 
       let h4 = document.createElement("h4");
-      h4.innerText = result.data.product.title;
+      h4.innerText = product.title;
 
       let description = document.createElement("p");
-      description.innerText = result.data.product.description;
+      description.innerText = product.description;
+
+      let button = document.createElement("button");
+      button.innerText = `Purchase - $${product.price}`;
+      button.setAttribute("data-sellix-product", product.uniqid)
+      button.type = "submit"
+      button.alt = "Buy Now with sellix.io";
 
       productDiv.appendChild(image);
       productDiv.appendChild(h4);
       productDiv.appendChild(description);
+      productDiv.appendChild(button);
 
       productContainer.appendChild(productDiv);
     });
+}
+
+function search(event) {
+  let searchTerm = event.target.value;
+  console.log(searchTerm)
+  let products = document.querySelectorAll(".product-div");
+  products.forEach((product) => {
+    const title = product.dataset.title;
+    if (title.length === 0)
+      product.style.display = "block";
+    else if (!title.includes(searchTerm))
+      product.style.display = "none";
+    else
+      product.style.display = "block";
+  })
 }
